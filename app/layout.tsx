ff4c4d474f7feb87e3d9a8d2c1f5e6858167dc54
Sergey Kancher <139557130+kancher@users.noreply.github.com>
2025-10-24 20:38:40 +0300

    Update layout.tsx
    Поправка отображения для iPhone  увеличение при фокусе на input!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+// app/layout.tsx
 import './globals.css'  
 
 export const metadata = {
   title: 'sMeNa.Tv',
   description: 'Народное телевидение нового поколения',
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 }
 
 export default function RootLayout({
