6be77701453f119d57ffb995daf4bf7bd4de1d95
Sergey Kancher <139557130+kancher@users.noreply.github.com>
2025-10-25 13:16:43 +0300

    Update layout.tsx
    Добавление Серёжи Гугл

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import YandexMetrika from '../components/YandexMetrika'
+import GoogleAnalytics from '../components/GoogleAnalytics'
 
 export const metadata = {
   title: 'sMeNa.Tv',
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
         <YandexMetrika />
+        <GoogleAnalytics />
       </body>
     </html>
   )
