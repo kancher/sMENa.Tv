c07f9e411dd7ee702dd4ad88175c5c1ebd0e2c17
Sergey Kancher <139557130+kancher@users.noreply.github.com>
2025-11-05 18:11:42 +0300

    Update page.tsx
    Попытка №07

--- app/kulya2/page.tsx
+++ app/kulya2/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 export default function Test() {
-  const API_URL = process.env.PUBLIC_BACKEND_API;
+  const API_URL = import.meta.env.PUBLIC_BACKEND_API;
   return <div>API: {API_URL || 'NOT FOUND'}</div>;
 }
